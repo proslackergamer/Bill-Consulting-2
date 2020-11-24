cd35017b460a95e4d063753ad71b3e5619cad82f
sebastianisabella <71610972+sebastianisabella@users.noreply.github.com>
2020-11-24 11:26:58 -0600

    Create home.php

---
+++ php/home.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include '../ssi/head.ssi'; ?>  
+  </head>
+  <body>
+      
+      <!-- ==========================  TOP MENU =========================== -->
+      <?php include '../ssi/nav.ssi'; ?>
+    
+       <!-- ==========================  MAIN CONTENT ====================== -->
+       <main> 
+       <!-- ==========================  JUMBOTRON ====================== -->
+       <!-- Main jumbotron for a primary marketing message or call to action -->
+          <?php include '../ssi/jumbotron.ssi'; ?> 
+       <!-- ============== Main Container ====================== --> 
+          <div class="container">
+            <!-- ==============  Row 2 ============================= -->
+            <div class="row">
+              <div class="col-md-4">
+                 <div class="thumbnail">
+                    <p class="mycenter"><img class="mycenter" src="../images/bananas.jpg" alt="..."></p>
+                    <div class="caption">
+                        <h3 class="mycenter">Bananas in Produce</h3>
+                        <p>
+                            Add flavor to your day with delicious bananas, always ripe and in season at your local Walmart! We guarantee freshness that will brighten up your day!
+                        </p>
+                        
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="thumbnail" >
+                    <p class="mycenter"><img src="../images/decadence.jpg" alt="..."></p>
+                    <div class="caption">
+                        <h3 class="mycenter">Decadence</h3>
+                        <p>
+                            Indulgence is only one step away at your local Walmart! Visit today to satisfy your sweet tooth!
+                        </p>
+                        
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                 <div class="thumbnail">
+                    <p class="mycenter"><img src="../images/bacon.png" alt="..."></p>
+                    <div class="caption">
+                        <h3 class="mycenter">Protein</h3>
+                        <p>
+                            Satisfy your meaty cravings by visiting Walmart! We have high quality cuts of protein and a wide selection to choose from, stop by and look today!
+                        </p>
+                        
+                    </div>
+                </div>
+             </div>
+            </div>
+            <hr>
+           <!-- ====================================================== -->
+           <!-- =================  Row 2 =================================== -->
+         <div class="row">
+            <div class="col-md-12">
+                <h2 class="rowheader">Other areas to explore . . . </h2>
+                <h3>Visit our Bakery</h3>
+                <p class="pright">
+                    Make a trip to your local Walmart bakery today to get freshly made bread and desserts.  It's only one stop away!
+                </p>
+                <h3>
+                    Ready to go meals
+                </h3>
+                <p class="pright">
+                    Need help? We've got you covered! Check out our section of ready to go meals that are prepared fresh daily, just waiting for you to take them home! 
+                </p>
+                <h3>
+                    Snacks
+                </h3>
+                <p class="pright">
+                    Whether it's for a day of adventure, a movie night, or to fuel your day with energy, Walmart has you covered.  We have a wide variety of snacks to meet everyone's individual needs.  Sweet and Salty? We've got that! Crunchy? We've got that too! Special combinations of items specifically catered to your desires.  Visit our snack section today!
+                </p>
+                <h3>
+                    Deli
+                </h3>
+                <p class="pright">
+                    Fresh meats and cheeses right at your fingertips! All you have to do is spend make a quick stop at our deli section. We have sandwiches made, or you can purchase individual ingredients to make your own creations like the chefs we know you are!
+                </p>
+            </div>
+
+        
+        </div>
+            <hr> 
+          </div> <!-- /container --> 
+        </main>
+
+      <!-- ==========================  FOOTER ============================= --> 
+      <?php include '../ssi/footer.ssi'; ?> 
+
+      
+       <!-- ==========================  JAVASCRIPTS ========================= -->
+       <!-- ========================== Custom =============================== -->
+       
+       <!-- ========================== JQuery, Popper, Bootstrap JS ========= -->
+       
+        
+
+    </body>
+</html>
