0d9b67ebd3cabf09cec00e20911406b25b486486
proslackergamer <67768546+proslackergamer@users.noreply.github.com>
2020-11-24 20:03:59 +0000

    Add files via upload

---
+++ php/team.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include '../ssi/head.ssi'; ?> 
+    <!-- Styles for this page only -->
+    <style>
+
+    h1 {
+    padding-left: 25px;
+    text-align: center;
+    }
+    
+    h3 {
+    text-align: center;
+    }
+    
+    p {
+    padding-left: 25px;
+    padding-right: 25px;
+    text-align: center;
+    }  
+    
+    p2 {
+    padding-left: 25px;
+    color: white;
+    }  
+    
+    </style>
+</head>
+    <!-- =============== Load an external font =================== -->
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+
+<!-- =============== BODY SECTION =================== --> 
+<body>
+    <!-- ===================== MENU ====================== -->
+    <?php include '../ssi/nav.ssi'; ?>   
+         <!-- ==========================  MAIN CONTENT ====================== -->
+     <main>
+       <!-- ==========================  JUMBOTRON ====================== -->
+       <!-- Main jumbotron for primary marketing message -->
+          <div class="jumbotron">
+            <div class="container">
+
+              <h2 class="display-3">The Changelog</h2>
+              <p2>Check out what we've done!</p2>
+
+            </div>
+          </div> 
+    <!-- This is a space to write a changelog for what each user has contributed -->
+         <h1>Meet our team!</h1><br>
+         <h3>What we've worked on:</h3>
+         
+         <p><strong>Isabella Sebastain:</strong> Created the "Home" and "Register" pages.  Made large CSS contributions, and coordinated meeting times for group.</p><br>
+
+         <p><strong>Lakyn Piotrowski:</strong> Assembled the "Product" and "Error" pages.  Made large CSS contributions, and found images for use within the project.  </p><br>
+
+         <p><strong>Lexie Markert:</strong>  Made the "Contact" and "Login" pages.  Designed Bill Consulting logo.  Also created and communicated information about Zoom meetings.  Completed proofreading of our pages.</p><br>
+
+         <p><strong>Baden Wackerle:</strong> Put together the "About" and "Team" pages.  Maintained central location (github) of code storage and communication. Made small CSS contributions, adjusted code templates for footer and menu.</p>
+         <br><br>
+         
+         
+         <h3>Meeting notes:</h3>
+         <p><strong>9/20/20:</strong>  The primary goal of the meeting was assigning which pages each person should have.  All group members were present, which made deciding which pages each person wanted easy.  We also talked about how we wanted to approach the sitewide CSS file, along with how we wanted to "store" our code for access.</p>
+         
+         <p><strong>9/29/20:</strong> The main goal for meeting was to further develop what we wanted to do for sitewide styling.  In addition, we set a standard footer and menu, as well as helped each other out with the various issues we ran into while creating our pages.  All members also attended this meeting.</p>
+        
+         <p><strong>10/3/20:</strong> Discussing final code adjustments, going through Lexie's proofreading.
+         </p>
+         
+         <p><strong>10/4/20:</strong> Final discussion of style, reviewed the references "about" and contributions "team" page.  Standardized the menu layout.
+         </p>
+         
+<!-- ===================== FOOTER ==================== -->
+<?php include '../ssi/footer.ssi'; ?>
+
+    </main>
+    </body>
+</html>
