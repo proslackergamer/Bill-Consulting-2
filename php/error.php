9a6f1f9cd10ee05a7b64efdd1e91e5d2dd1484fd
piotrol1 <71610861+piotrol1@users.noreply.github.com>
2020-11-28 18:07:21 -0500

    Create error.php

---
+++ php/error.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+ <?php include '../ssi/head.ssi'; ?>  
+  <body>
+      
+      <!-- ==========================  TOP MENU =========================== -->
+ <?php include '../ssi/nav.ssi'; ?> 
+       <!-- ==========================  MAIN CONTENT ====================== -->
+     <main role="main">
+
+  <!-- Main jumbotron for a primary marketing message or call to action -->
+  <div class="jumbotron">
+            <div class="container">
+
+                <h2 class="display-3">Uh Oh :(</h2>
+
+                <h3 style = "color: white;" >Sorry there has been an error</h3>
+
+
+    </div>
+  </div>
+
+  
+</main>
+
+      <!-- ==========================  FOOTER ============================= --> 
+      <?php include '../ssi/footer.ssi'; ?> 
+
+    </body>
+</html>
