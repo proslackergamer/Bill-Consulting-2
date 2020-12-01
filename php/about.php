9dbd0d095e37c06f4728446c6a3c64467724f6e8
proslackergamer <67768546+proslackergamer@users.noreply.github.com>
2020-12-01 04:55:30 +0000

    Create about.php

---
+++ php/about.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php include '../ssi/head.ssi'; ?>
+    <!-- =============== Custom styles, this page only =================== -->
+    <style>
+    h1 {
+    padding-left: 25px;
+    text-align: center;
+    padding-top 25px;
+    }
+    
+    h2 {
+    padding-left: 25px;
+    }
+    
+    p {
+    padding-left: 25px;
+    }  
+        
+    div {
+    padding-top 25px;
+    }
+           
+   </style>
+        
+</head>
+        
+    
+    
+<!-- =============== BODY SECTION =================== --> 
+<body>
+   <!-- ===================== MENU ====================== -->
+<?php include '../ssi/nav.ssi'; ?>
+    
+<!-- ==========================  MAIN CONTENT ====================== -->
+<main>
+    <?php include '../ssi/jumbotron.ssi'; ?>
+    <br><br><br><br><h1>Our References</h1><br><br>
+    
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Walmart Logos</h2>
+            <p>(2018, December 11). In Walmart. Retrieved October 1, 2020, from https://one.walmart.com/content/people-experience/associate-brand-center/downloads0/assets/logos.html</p>
+            
+            <h2>Bacon Image</h2>
+            <p>Transparent Bacon High Resolution - Kevin Bacon on Bacon (n.d.). In Nice PNG. Retrieved from https://www.nicepng.com/ourpic/u2a9o0o0y3e6t4q8_transparent-bacon-high-resolution-kevin-bacon-on-bacon/
+            
+            <h2>Cupcake Image</h2>
+            <p>Cupcakes! XL. (n.d.). In Cupcakes! Bake &amp; Decorate by Maverick Software LLC. Retrieved from https://appadvice.com/app/cupcakes-bake-decorate/347362622</p>
+        </div>
+        
+        <div class="col-md-4">
+            <h2>Bill Consulting Logo</h2>
+            <p>Created by Markert, Lexie. (Artist). (2020). Bill Consulting Logo. [Digital].</p>
+        
+        
+            <h2>Donut Image</h2>
+            <p>Delish, US. (n.d.). Donut. In Delish. Retrieved from https://www.delish.com/cooking/recipe-ideas/a24788319/how-to-make-donuts-at-home/</p>
+        
+            <h2>Banana Image</h2>
+            <p>Bananas &amp; Plantains. (n.d.). In Foothills IGA. Retrieved from https://www.foothillsiga.com/shop/produce/fresh_fruit/bananas_and_plantains/d/235910</p>
+        </div> 
+        
+        <div class="col-md-4">
+            <h2>Cookie Image</h2>
+            <p>Feierback, P. (n.d.). Mrs Fields Cookies Recipe. In Delish. Retrieved from https://www.delish.com/cooking/a25487454/mrs-fields-cookies-recipe/</p>
+        
+            <h2>Cupcake Image</h2>
+            <p>Chrisse (No last name given). (n.d.). Best Ever Chocolate Cupcakes from Scratch. In The Busy Baker. Retrieved from https://thebusybaker.ca/best-ever-chocolate-cupcakes-from-scratch/</p>
+        </div> 
+    </div>  
+    
+    
+
+
+        
+</main>
+ </body>
+    <!-- ===================== FOOTER ==================== -->
+    <?php include '../ssi/footer.ssi'; ?>
+</html>
