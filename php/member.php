bbc17baa26287603e96ce0d1b5261bfe88539f28
proslackergamer <67768546+proslackergamer@users.noreply.github.com>
2020-12-01 04:47:28 +0000

    Create member.php

---
+++ php/member.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    
+<head>
+    <?php include '../ssi/head.ssi'; ?>
+    
+    <!-- Styles for this page only --> 
+    <style>
+        .formcontrol {text-align: center;}
+    </style>
+   
+</head>
+ <body>
+  <?php include '../ssi/nav.ssi'; ?>  
+    <main>
+    
+    
+    </main>
+   </body>
+  <?php include '../ssi/footer.ssi'; ?>
+ </body>
+</html>
